293a4c218de3624cce12d4935fe1b7beaf9e6343
Simul <simulbista@gmail.com>
2023-06-30 21:58:18 -0400

    added data.sql with 5 insert queries which populates the in memory db when the app launches every time, made changes to application.properties for that

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO BOOK (AUTHOR,GENRE,NAME,PRICE)
+VALUES ('Sir Arthur Conan Doyle' , 'Mystery', 'The Case Book of Sherlock Holmes' ,200.0),
+('Sir Arthur Conan Doyle' , 'Mystery', 'The memoirs of Sherlock Holmes' ,200.0),
+('W.E. Johns', 'Mystery' , 'Biggles in France' ,120.0),
+('Makoto Shinkai' , 'Life' , 'Your Name' ,180.0),
+('Makoto Shinkai' , 'Life', '5 Centimeters per second' ,45.0);
